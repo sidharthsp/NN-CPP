86f1ee97dfb744a9fefd42fa34caa8e38d1e3301
sidharthsp <sidharth.s.prashanth@gmail.com>
2023-08-11 21:06:46 +0100

    added class for Multilayer perceptron

--- neuron.cpp
+++ neuron.cpp
@@ -25,3 +25,40 @@ double Perceptron::sigmoid(double sum){
     return 1.0/(exp(-sum)+1.0);
 }
 
+
+MultiLayerPerceptron::MultiLayerPerceptron(vector<int> layers, double bias, double eta){
+    this->bias = bias;
+    this->layers = layers;
+    this->eta = eta;
+    for (size_t i = 0; i < layers.size(); i++)
+    {
+        values.push_back(vector<double>(layers[i],0.0));
+        network.push_back(vector<Perceptron>());
+
+        //first layer is empty as it has no neurons
+
+        if (i > 0){
+            for (size_t j = 0; j < layers[i]; j++)
+            {
+                network[i].push_back(Perceptron(layers[i-1],bias));
+            }
+            
+        }
+
+    }
+
+}
+
+void MultiLayerPerceptron::print_weights(){
+
+for (size_t i = 0; i < layers.size(); i++)
+{
+    for (size_t j = 0;j < layers[i]; j++)
+    {
+        cout << "Layer: ";
+    }
+    
+}
+
+
+}
